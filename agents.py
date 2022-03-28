87d7e3ead19af6996e1f459235b16dbddbe92e0d
Victor Putrich <scherer.victor98@gmail.com>
2022-03-28 19:32:32 -0300

    starting SHOT algorithm

--- agents.py
+++ agents.py
@@ -99,4 +99,86 @@ class SequentialHalvingAgent(Agent):
     def get_move(self, board):
         return self.sequential_halving(board, 200)
 
+class SHOT(Agent):
     
+    #not sure if I update parameters from TTable or from the stack
+    #not sure whats node's body (it looks like each node has a list of legal movements)
+    def sh_over_trees(board, budget, u_budget, playouts, wins, player):
+
+        legal_moves = tictactoe.list_legal_moves(board, player)
+        
+        # if is terminal check who the outcome and update variables
+        if tictactoe.is_terminal_state(board):
+            TTable[board].wins += 1 if tictactoe.is_victory(board, self.player) else 0
+            TTable.playouts +=1 # idk why it suposed to update these variables
+            return
+        
+        # if there is only one playout left, roll it and update variables
+        if bugdget == 1:
+            result = playout(board)
+            wins += result
+            playouts+=1
+            u_budget+=1
+
+            TTable[board].wins += result
+            TTable[board].playouts+=1
+            TTable[board].u_budget+=1
+            budget -= 1
+            return
+
+        # if there is only one move to make, make it and then update variables.
+        if len(legal_moves) == 1:
+            u=0, p=0, w=0
+            n_board = tictactoe.make_move(board, legal_moves[0])
+            sh_over_trees(n_board, budget, u, p, w, player*-1)
+            TTable[n_board].playouts+=1
+            TTable[n_board].u_budget+=1
+            budget -=1
+            # Here I have to update wins but idk how
+            return legal_moves[0]
+
+
+    
+        t = TTable[board] # not sure (key?)
+        # if there is less legal_moves than playout budgets distribute it beetwen new legal_moves
+        if t.budget_node <= len(legal_moves):
+            M_zero = [m for m in legal_moves if m not in TTable]
+            count = 0
+            for m in M_zero:
+                count += 1
+                n_board = tictactoe.make_move(board, m) 
+                result = playout(n_board)
+                TTable[n_board].playouts+=1
+                TTable[n_board].u_budget+=1
+                TTable[n_board].wins += (1 if result == self.player else 0)
+                
+                if budget == count:
+                    break
+        sort_moves()
+
+        # Sequential Halving
+        b = 0
+        while len(legal_moves)>1:
+            b = b + max(1, floor( (t.budget_node + budget) / (len(legal_moves)* log(tictactoe.list_legal_moves(board, player)) )
+            for m in legal_moves:
+                if t.playouts[m] < b:
+                    bl = b - t.playouts[m]
+                    
+                    # ??? idk
+                    if t == root and len(legal_moves) == 2 and m == S[0]:
+                        bl = budget - u_budget - (b - t.playouts[b[1]])
+                    
+                        bl = min(bl, budget - budgetUsed)
+                    u = 0, p = 0, w = 0
+                    n_board = tictactoe.make_move(board, m)
+                    sh_over_trees(n_board, bk, u, p, w, agent*-1)
+                    playouts += 1
+                    u_used += 1
+                    # wins = ?
+                    # have to update t too
+            if u_budget >= budget:
+                break
+            sort_moves(legal_moves)
+            halve(legal_moves)
+        #update t.budgetNode
+        return legal_moves[0]
