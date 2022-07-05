6a6030445fffae5ae6561b2bae048688efa5cfa7
Victor Scherer Putrich <scherer.victor98@gmail.com>
2022-07-05 19:52:19 -0300

    Update CanonicalShot.py

--- src/agents/CanonicalShot.py
+++ src/agents/CanonicalShot.py
@@ -1,9 +1,8 @@
+from audioop import reverse
 import math
 import copy
 from agents.agent import Agent
 from agents.sequential_halving import SequentialHalvingAgent
-
-
 
 
 class SHOTNode():
@@ -15,15 +14,30 @@ class SHOTNode():
         self.wins   = 0
         self.playouts = 0
         self.children = [] # childre apply(available_action, state)
-    
-    # overload '>' operator
-    def __gt__(a, b):
-        return a if a.wins/a.playouts > b.wins/b.playouts else b
-        
+'''
+    Why improve 'Canonical' version of SHOT ? 
+        1) The original version of the algorithm is too complex and the pseudo-code is not clear. 
+        2) It uses transposition table as data structure (not intuitive) and pass parameters by reference. 
+        3) The searching method has three terminal cases. Two of them does basically the same thing. 
+        Conceptually has a little difference: It considers not making rollouts when the node is terminal. We believe
+        that it can be considered as a rollout and the work that is done there only returns the Utility value.
+        4) It is difficult to understand the different steps of the algorithm: everything is done in one function.
+        5) Inside the recursion calls for each halvening loop, it has an 'if' condition that holds only at the
+        root node when there is two moves left.
+        6) It returns the best move for each time it closes a recursion call on the stack but only use it at the root
+        in the last halve.
+        7) The work done in SHOT algorithm have comparisions with UCT only for one game and we dont know how it works for
+        general game playing.
+                            
+    * Our objective is to make a version that we believe is simpler and better to understand. We believe that SHOT
+      could be used as a General Game Play algorithm but the way it was presented first time is too complex to be
+      the canonical form of the algorithm.
+
+'''
 
 class CanonicalShot(SequentialHalvingAgent):
     def select_action(self, game, context, max_iterations=0, max_episodes=0, max_depth=0):
-        root = SHOTNode(context, None, game.moves(context, self.player), self.get_opponent_of(self.player))
+        root = SHOTNode(context, None, game.moves(context, self.player), self.player)
         self.game = game
         budget = 1000
         budget_used = self.expand(game, root, 1000)
@@ -41,8 +55,6 @@ class CanonicalShot(SequentialHalvingAgent):
                 ch = root.children[i]
                 child_budget = (layer_budget/virtual_ch_len) - ch.playouts
 
-                # Aqui faz aquele negócio estranho para quando tem só |S| == 2
-                # aqui é para alocal o resto do budget para cada
                 if virtual_ch_len <= 2:
                     child_budget = budget - root.playouts - child_budget
 
@@ -55,11 +67,17 @@ class CanonicalShot(SequentialHalvingAgent):
         return root.children[0]
 
     def search(self, game, node, budget):
+        
         if self.game.is_terminal_state(node.state) or budget == 1:
             node.wins += self.make_playout(node.state, self.player)
             node.playouts += 1
             return
         
+        if len(node.children) == 1:
+            self.search(game, node.children[0], budget-1)
+            return
+
+
         if len(node.unvisited_actions) > 0:
             self.expand(game, node, budget)
             if node.playouts >= budget:
@@ -102,7 +120,6 @@ class CanonicalShot(SequentialHalvingAgent):
         return budget_used
 
     def layer_budget(self, trials, budget, len_children):
-        
         total_budget = trials + budget           
         total_layers = math.ceil(math.log(len_children, 2))
         return math.floor(max(1, total_budget/total_layers))
@@ -110,18 +127,14 @@ class CanonicalShot(SequentialHalvingAgent):
 
     def sort(self, array, interval, player):
         ch_sort_cpy = array[:interval]
+            
         if self.player == player:
-            ch_sort_cpy.sort()
+            ch_sort_cpy.sort(key=lambda ch: ch.wins/ch.playouts, reverse = True)
         else:
-            ch_sort_cpy.sort(reverse = True)
+            ch_sort_cpy.sort(key=lambda ch: ch.wins/ch.playouts)
         
         array = ch_sort_cpy + array[interval:]
         return array
 
     def get_name(self):
         return "SHOT Agent"
-
-
-
-
-
